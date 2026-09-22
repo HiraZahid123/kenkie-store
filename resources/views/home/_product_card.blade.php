654da378b985bdca3ce4b59f31e42b17ff1bbda2
HiraZahid123 <compilelogics@.com>
2026-09-22 17:28:54 +0500

    Remove compare icon from product cards in home and shop views

--- resources/views/home/_product_card.blade.php
+++ resources/views/home/_product_card.blade.php
@@ -27,17 +27,6 @@
                         </a>
                     </div>
                 </div>
-                <a href="#" class="compare icon-svg" data-product_id="{{ $product->id }}" rel="nofollow" title="Compare">
-                    <svg width="40px" height="40px" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M0 0h48v48H0z" fill="none"/>
-                        <g id="Shopicon">
-                            <path d="M44,14H25.738C24.848,10.551,21.726,8,18,8s-6.848,2.551-7.738,6H4v4h6.262c0.889,3.449,4.011,6,7.738,6
-                                s6.848-2.551,7.738-6H44V14z M18,20c-2.206,0-4-1.794-4-4c0-2.206,1.794-4,4-4s4,1.794,4,4C22,18.206,20.206,20,18,20z"/>
-                            <path d="M44,30h-6.262c-0.889-3.449-4.011-6-7.738-6s-6.848,2.551-7.738,6H4v4h18.262c0.889,3.449,4.011,6,7.738,6
-                                s6.848-2.551,7.738-6H44V30z M30,36c-2.206,0-4-1.794-4-4c0-2.206,1.794-4,4-4s4,1.794,4,4C34,34.206,32.206,36,30,36z"/>
-                        </g>
-                    </svg>
-                </a>
                 <a href="{{ $link }}" data-product_id="{{ $product->id }}" title="Quick view" class="sw-quickview icon-svg" data-type="quickview">
                     <svg width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
